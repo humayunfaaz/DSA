e9554950c522987235ebcfc9417e5e427e0bdbdf
Ali Faaz <humayunfaaz@gmail.com>
2026-08-15 22:20:31 +0530

    more change

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -2,33 +2,22 @@
 using namespace std;
 
 int main() {
- int n ;
-    cin>> n ;
-    int a[n]; 
-    for (int i = 0; i < n; i++) {
-        cout << "Enter element " << i + 1 << ": ";
-        cin >> a[i];
-    }
-
-   
+    int n;
+    cin >> n;           // Let's say n = 5
+    int a[n];
+    
+    // Input: [5, 2, 4, 1, 3]
+    
     for (int i = 1; i < n; i++) {
-   
         int j = i;
-
-        while (j >= 0 && a[j-1] > a[j]) {
-              int temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
+        while (j >= 1 && a[j-1] > a[j]) {
+            int temp = a[j];
+            a[j] = a[j-1];
+            a[j-1] = temp;
             j--;
         }
-
-        
-
-    cout << "Sorted array: ";
-
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
     }
-
+    
+    // Output sorted array
     return 0;
 }
